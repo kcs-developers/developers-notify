6f853d9f6d0ad29b55266e7020334462ac20ae22
paduck-96 <peterrabitt1234@naver.com>
2023-04-18 06:36:24 +0900

    fix: cors error

--- src/main/java/com/developers/notify/config/WebMvcConfig.java
+++ src/main/java/com/developers/notify/config/WebMvcConfig.java
@@ -29,9 +29,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 적용
-                .allowedOrigins("*") // 모든 도메인 허용
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
-                .allowedHeaders("*")
+                .allowCredentials(true) // credentials 허용 설정 추가
                 .maxAge(3600); // 캐시 유효 시간 설정
     }
 }
