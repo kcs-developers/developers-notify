aafe4d988dce6788469393ba1420b99fff826b8f
paduck-96 <peterrabitt1234@naver.com>
2023-04-21 21:34:42 +0900

    fix: cors 변경

--- src/main/java/com/developers/notify/config/WebMvcConfig.java
+++ src/main/java/com/developers/notify/config/WebMvcConfig.java
@@ -29,7 +29,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 적용
-                .allowedOrigins("/*")
+                .allowedOrigins("*")
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowCredentials(true) // credentials 허용 설정 추가
                 .maxAge(3600); // 캐시 유효 시간 설정
